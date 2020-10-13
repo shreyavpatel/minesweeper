57d0a3e52526361cc03dab5a0cc5e81c916c8358
shreyavpatel <shreya984@hotmail.com>
2020-10-12 22:52:31 -0600

    Updates

---
+++ precache-manifest.2de6e7b10b9b33965efa32fbe114440a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947959ceec9cf34ad56a2d10d5ea8d45",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "044b586215ddb2e6ae8e",
+    "url": "/minesweeper/static/css/main.3cb1de56.chunk.css"
+  },
+  {
+    "revision": "e3b8f510c31b37dd1a14",
+    "url": "/minesweeper/static/js/2.dff36062.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper/static/js/2.dff36062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "044b586215ddb2e6ae8e",
+    "url": "/minesweeper/static/js/main.d99c58cd.chunk.js"
+  },
+  {
+    "revision": "03fdde6e651e9bdac82d",
+    "url": "/minesweeper/static/js/runtime-main.9c76a611.js"
+  }
+]);
